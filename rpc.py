2a92bc8399507dc9056792ad391bd966c3be9ba9
ytrsoft <1654486219@qq.com>
2025-02-27 23:33:54 +0800

    back

--- rpc.py
+++ rpc.py
@@ -4,19 +4,10 @@ def read_script(name):
     with open(f"{name}.js", 'r', encoding='utf8') as f:
         return f.read()
 
-
-class Wrap:
-    def __init__(self, script):
-        self.script = script
-        script.load()
-        self.rpc = script.exports_sync
-
-    def unload(self):
-        self.script.unload()
-
-def create_momo():
+def make_rpc():
     device = frida.get_usb_device()
     session = device.attach('MOMO陌陌')
     code = read_script('rpc')
     script = session.create_script(code)
-    return Wrap(script)
+    script.load()
+    return script
