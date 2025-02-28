e44dc87fa0029b5ad6c517824fb6625c8491966d
ytrsoft <1654486219@qq.com>
2025-02-28 17:48:09 +0800

    apk sign

---
+++ sign.java
@@ -0,0 +1,14 @@
+/*生成apk签名*/
+public static String O() throws Exception {
+  try {
+      Signature S = S();
+      if (S != null) {
+          return com.immomo.mmutil.m.b(S.toCharsString());
+      }
+      return null;
+  } catch (Exception unused) {
+      return null;
+  }
+}
+
+
