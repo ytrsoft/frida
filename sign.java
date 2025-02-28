4ff201b4b1ceda5d2a0bffa11a0d0ae6c80631fb
ytrsoft <1654486219@qq.com>
2025-02-28 22:21:16 +0800

    infos

--- sign.java
+++ sign.java
@@ -7,6 +7,18 @@
 //     return-object v0
 // .end method
 
+// {
+//   "username": "momoid",
+//   "session": "login session",
+//   "resource": "android",
+//   "cflag": "24b",
+//   "uid": "ea783e5dfa32dd306b0861d2e6d7f4ef",
+//   "version": 12715,
+//   "extraData": {
+//     "abtest": "nearbypeopleliveexp-kmjyjy_blank;aisayhi-bmhtje_blank;test-rsxyxo_blank;morenew-wkhqld_A;microcosm-jtgdzn_blank;active-wklfmo_blank;location-vwzlkp_A;nearbyfeedlive-mclxen_blank"
+//   }
+// }
+
 public static String O() throws Exception {
   try {
       Signature S = S();
