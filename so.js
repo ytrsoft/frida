54c3b8cc112536110fda3bc14477fc2384a0c4b9
ytrsoft <1654486219@qq.com>
2025-03-03 00:42:22 +0800

    log ip

--- so.js
+++ so.js
@@ -1,3 +1,9 @@
-Java.perform(() => {
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>')
-})
+const inet_pton = Module.findExportByName('libc.so', 'inet_pton')
+console.log(inet_pton)
+if (inet_pton != null) {
+  Interceptor.attach(inet_pton, {
+    onEnter: (args) => {
+      console.log(args[1].readUtf8String())
+    }
+  })
+}
