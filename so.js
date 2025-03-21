7ef3e0d9f85ef6781fdb9e309a667b369017e46f
ytrsoft <1654486219@qq.com>
2025-03-21 11:35:44 +0800

    sendto

--- so.js
+++ so.js
@@ -1,9 +1,24 @@
-const inet_pton = Module.findExportByName('libc.so', 'inet_pton')
-console.log(inet_pton)
-if (inet_pton != null) {
-  Interceptor.attach(inet_pton, {
-    onEnter: (args) => {
-      console.log(args[1].readUtf8String())
+const attachLibc = (name, handle) => {
+  const addr = Module.findExportByName('libc.so', name)
+  Interceptor.attach(addr, {
+    onEnter(args) {
+      this.arg0 = args[0]
+      this.arg1 = args[1]
+      this.arg2 = args[2]
+      this.arg4 = args[4]
+    },
+    onLeave() {
+      const size = this.arg2.toInt32()
+      if (size > 0) {
+        handle && handle(this)
+      }
     }
   })
 }
+
+attachLibc('sendto', (ctx) => {
+  const size = ctx.arg2.toInt32()
+  if (size > 0) {
+    console.log(hexdump(ctx.arg1, { length: size }))
+  }
+})
