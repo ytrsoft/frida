8eae490f288c1b7a66ffe310ff125a47ec57e924
ytrsoft <1654486219@qq.com>
2025-03-21 11:45:54 +0800

    sendto

--- so.js
+++ so.js
@@ -20,10 +20,23 @@ const charLine = (char) => {
   return Array(100).fill(char).join('')
 }
 
+const socket = (fd) => {
+  const type = Socket.type(fd)
+  if (type !== null) {
+    const peer = Socket.peerAddress(fd)
+    const local = Socket.localAddress(fd)
+    if (peer.ip && peer.port && local.ip && local.port) {
+      console.log(`${peer.ip}:${peer.port} => ${local.ip}:${local.port}`)
+    }
+  }
+}
+
 attachLibc('sendto', (ctx) => {
+  const fd = ctx.arg0.toInt32()
   const size = ctx.arg2.toInt32()
   if (size > 0) {
     console.log(charLine('>'))
+    socket(fd)
     console.log(hexdump(ctx.arg1, { length: size }))
     console.log(charLine('<'))
   }
